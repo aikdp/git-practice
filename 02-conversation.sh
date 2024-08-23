fda6ef2432a1bfedb53f1f045ae6da59eebd41e6
aikdp <kdprasad028@gmail.com>
2024-08-23 19:43:50 +0530

    Created conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine, how are you doing?"
+echo "Ramesh:: I am doing good Suresh, whats going on?"
+echo "Suresh:: I started learning shell script"
