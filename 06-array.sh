4e0d5eac6ee95e36080d5e6b8172374c2ec05af5
aikdp <kdprasad028@gmail.com>
2024-08-23 19:57:37 +0530

    Created Datatypes script with srtings

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "first fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "All fruit is: ${FRUITS[@]}"
