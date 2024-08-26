a3ae1f6e10399d9123d02c5a83d37fe4a013f003
aikdp <kdprasad028@gmail.com>
2024-08-26 11:01:08 +0530

    Variables with read command added

---
+++ 07-variables-password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read -s USERNAME
+echo "Entered username is:: $USERNAME"
+echo "Please enter password::"
+read -s PASSWORD
+echo "Enetered password is:: $PASSWORD"
