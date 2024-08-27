48c9c67b31df497e6590449ef5c1f0710a3c4559
aikdp <kdprasad028@gmail.com>
2024-08-27 11:36:00 +0530

    Special Variables

--- 13-functions-function.sh
+++ 13-functions-function.sh
@@ -50,6 +50,56 @@
 # fi   
 
 
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# VALIDATE(){
+# if [ $? -ne 0 ]
+#     then
+#         echo -e " $1 command is... $R ERROR $N"
+#         exit 1
+#     else   
+#         echo -e "Installing $1... $G SUCCESS $N"
+#     fi
+# }
+
+# #check user has ROOT access or not
+# if [ $USERID -ne 0 ]
+# then
+#     echo -e " $R You don't have root acces, please login as ROOT USER $N"
+#     exit 1
+# fi
+
+# #Neat and clean script with MYSQL INSTALLATION
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo -e " $R MYSQL is not installed please Install MYSQL $N"
+
+#     dnf install mysql -y 
+#     VALIDATE $? 
+
+# else 
+#     echo -e " $Y MYSQL already installed $N"
+# fi    
+
+# dnf list installed git
+# if [ $? -ne 0 ]
+# then 
+#     echo -e " $R GIT is not installed please Install GIT $N"
+
+#     dnf install git -y 
+#      VALIDATE $? 
+
+# else 
+#     echo -e " $Y GIT already installed $N"
+# fi   
+
+
+#Checking and is=suing $1 $2 dollars 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -57,13 +107,13 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
-if [ $? -ne 0 ]
+if [ $1 -ne 0 ]
     then
-        echo -e " $1 command is... $R ERROR $N"
+        echo -e " $2 command is... $R ERROR $N"
         exit 1
     else   
-        echo -e "Installing $1... $G SUCCESS $N"
-    fi
+        echo -e " $2 is... $G SUCCESS $N"
+    fi 
 }
 
 #check user has ROOT access or not
@@ -80,7 +130,7 @@ then
     echo -e " $R MYSQL is not installed please Install MYSQL $N"
 
     dnf install mysql -y 
-    VALIDATE $? 
+    VALIDATE $? "Installing MYSQL"
 
 else 
     echo -e " $Y MYSQL already installed $N"
@@ -92,7 +142,7 @@ then
     echo -e " $R GIT is not installed please Install GIT $N"
 
     dnf install git -y 
-     VALIDATE $? 
+     VALIDATE $? "Installing GIT"
 
 else 
     echo -e " $Y GIT already installed $N"
