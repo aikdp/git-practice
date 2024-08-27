a8ccf8110e2a73d64fb4fd2203337f5537743389
aikdp <kdprasad028@gmail.com>
2024-08-27 06:55:03 +0530

    Special Variables

--- 09-ifcondtion.sh
+++ 09-ifcondtion.sh
@@ -4,8 +4,9 @@
 
 # echo "Today date is: $DATE"
 NUMBER=$1
-if[ $NUMBER -gt 17 ]; then
-    echo "Given: $NUMBER is greater than 17"
+if  $NUMBER -gt 17 ]
+then
+echo "Given: $NUMBER is greater than 17"
 else 
-    echo "Given: $NUMBER is lessthan 17"
+echo "Given: $NUMBER is lessthan 17"
 fi
