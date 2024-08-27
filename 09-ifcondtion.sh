875f63c5064558c8c2134a089fcc43e77b454f15
aikdp <kdprasad028@gmail.com>
2024-08-27 10:00:22 +0530

    Special Variables

--- 09-ifcondtion.sh
+++ 09-ifcondtion.sh
@@ -4,6 +4,7 @@
 
 # echo "Today date is: $DATE"
 
+#Given number is Greaterthan
 # NUMBER=$1
 # if [ $NUMBER -gt 17 ]
 # then
@@ -12,6 +13,7 @@
 # echo "Given: $NUMBER is lessthan 17"
 # fi
 
+#Given number is lessthan
 # NUMBER=$1
 # if [ $NUMBER -lt 17 ]
 # then
@@ -19,10 +21,21 @@
 # else 
 # echo "Given: $NUMBER is greater than 17"
 # fi
+
+#Given number is less than equal
+# NUMBER=$1
+# if [ $NUMBER -le 17 ]
+# then
+# echo "Given: $NUMBER is less than 17"
+# else 
+# echo "Given: $NUMBER is greater than 17"
+# fi
+
+#Given number is greater than equal
 NUMBER=$1
-if [ $NUMBER -le 17 ]
+if [ $NUMBER -ge 17 ]
 then
-echo "Given: $NUMBER is less than 17"
+echo "Given: $NUMBER is Greater than 17"
 else 
-echo "Given: $NUMBER is greater than 17"
+echo "Given: $NUMBER is less than 17"
 fi
