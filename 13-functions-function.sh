82f7b6c43e92e6de427e05ecd2b22743a0a070d8
aikdp <kdprasad028@gmail.com>
2024-08-27 11:21:01 +0530

    Special Variables

--- 13-functions-function.sh
+++ 13-functions-function.sh
@@ -1,6 +1,55 @@
 #!/bin/bash
 
 
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# VALIDATE(){
+# if [ $? -ne 0 ]
+#     then
+#         echo -e " $1 command is... $R ERROR $N"
+#         exit 1
+#     else   
+#         echo -e "Installing $1... $G SUCCESS $N"
+#     fi
+# }
+
+# #check user has ROOT access or not
+# if [ $USERID -ne 0 ]
+# then
+#     echo -e " $R You don't have root acces, please login as ROOT USER $N"
+#     exit 1
+# fi
+
+# #Neat and clean script with MYSQL INSTALLATION
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo -e " $R MYSQL is not installed please Install MYSQL $N"
+
+#     dnf install mysql -y 
+#     VALIDATE $? 
+
+# else 
+#     echo -e " $Y MYSQL already installed $N"
+# fi    
+
+# dnf list installed git
+# if [ $? -ne 0 ]
+# then 
+#     echo -e " $R GIT is not installed please Install GIT $N"
+
+#     dnf install git -y 
+#      VALIDATE $? 
+
+# else 
+#     echo -e " $Y GIT already installed $N"
+# fi   
+
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -30,7 +79,7 @@ if [ $? -ne 0 ]
 then 
     echo -e " $R MYSQL is not installed please Install MYSQL $N"
 
-    dnf install mysql -y 
+    dnf install mysqlgg -y 
     VALIDATE $? 
 
 else 
@@ -42,7 +91,7 @@ if [ $? -ne 0 ]
 then 
     echo -e " $R GIT is not installed please Install GIT $N"
 
-    dnf install git -y 
+    dnf install gitvg -y 
      VALIDATE $? 
 
 else 
