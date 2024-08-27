556699b9f62b3337f4d32123b68e6e9e822f3676
aikdp <kdprasad028@gmail.com>
2024-08-27 21:25:30 +0530

    loops concept

--- 15.loops.sh
+++ 15.loops.sh
@@ -3,7 +3,13 @@
 #echo "1 2 3 4 5 6 7 8 9 10"
 
 #printing o to 100 numbers
-for i in {0..100}
-do
+# for i in {0..100}
+# do
+#     echo "$i"
+# done
+
+#print 0 to 1000
+for i in {0..1000}
+do 
     echo "$i"
 done
