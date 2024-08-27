c2b65a3c68459f688e66a80109acdb9dc952357d
aikdp <kdprasad028@gmail.com>
2024-08-27 10:40:24 +0530

    Special Variables

--- 11-condtion-mysql-script.sh
+++ 11-condtion-mysql-script.sh
@@ -21,6 +21,38 @@
 #     echo -e "Installing GIT... \e[32m SUCCESS \e[0m"
 # fi
 
+# #chekc user has ROOT access or not
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo -e " $R You don't have root acces, please login as ROOT USER $N"
+#     exit 1
+# fi
+
+# #Neat and clean script
+# dnf list installed git
+# if [ $? -ne 0 ]
+# then 
+#     ech "GIT is not installed please Install GIT"
+
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo -e "command is... $R ERROR $N"
+#         exit 1
+#     else   
+#         echo -e "Installing GIT... $G SUCCESS $N"
+#     fi
+
+# else 
+#     echo -e " $Y GIT already installed $N"
+# fi    
+
 #chekc user has ROOT access or not
 USERID=$(id -u)
 R="\e[31m"
@@ -34,21 +66,21 @@ then
     exit 1
 fi
 
-#Neat and clean script
-dnf list installed git
+#Neat and clean script with MYSQL INSTALLATION
+dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-    ech "GIT is not installed please Install GIT"
+    ech "MYSQL is not installed please Install MYSQL"
 
-    dnf install git -y
+    dnf install mysql -y
     if [ $? -ne 0 ]
     then
         echo -e "command is... $R ERROR $N"
         exit 1
     else   
-        echo -e "Installing GIT... $G SUCCESS $N"
+        echo -e "Installing MYSQL... $G SUCCESS $N"
     fi
 
 else 
-    echo -e " $Y GIT already installed $N"
+    echo -e " $Y MYSQL already installed $N"
 fi    
