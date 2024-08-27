28c86092fb4adaa30f908747dd2b1998f4bd1bb6
aikdp <kdprasad028@gmail.com>
2024-08-27 11:11:05 +0530

    Special Variables

---
+++ 13-functions-function.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $? -ne 0 ]
+    then
+        echo -e " $1 command is... $R ERROR $N"
+        exit 1
+    else   
+        echo -e "Installing $1... $G SUCCESS $N"
+    fi
+}
+
+#check user has ROOT access or not
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R You don't have root acces, please login as ROOT USER $N"
+    exit 1
+fi
+
+#Neat and clean script with MYSQL INSTALLATION
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo -e " $R MYSQL is not installed please Install MYSQL $N"
+
+    dnf install mysql -y 
+    VALIDATE $? 
+
+else 
+    echo -e " $Y MYSQL already installed $N"
+fi    
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    echo -e " $R GIT is not installed please Install GIT $N"
+
+    dnf install git -y 
+     VALIDATE $? 
+
+else 
+    echo -e " $Y GIT already installed $N"
+fi   
