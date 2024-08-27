a82cf379ae94f6e7befe49a7e371ad01c8c6a63e
aikdp <kdprasad028@gmail.com>
2024-08-27 23:44:38 +0530

    loops concept

--- 17-loopslogfileservices.sh
+++ 17-loopslogfileservices.sh
@@ -29,7 +29,7 @@ CHECK(){
 ROOT(){
 if [ $USERID -ne 0 ]
     then    
-        echo -e "$R USER does not have root access, Please login as ROOT USER $N"| TEE -a $LOG_FILE
+        echo -e"$R USER does not have root access, Please login as ROOT USER $N"| TEE -a $LOG_FILE
         exit 1
     else    
         echo -e"$Y USER has ROOT ACCESS $N"| TEE -a $LOG_FILE
