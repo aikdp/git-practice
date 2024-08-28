1dc7321e3cf64ac048deb728f6471371df2bbb40
aikdp <kdprasad028@gmail.com>
2024-08-28 06:51:42 +0530

    devops siva session notes

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u)
 #/var/log/loops-log-file redirectors<timestamp>.log
-LOGS_FOLDER="/var/log/shell-script"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOG_FOLDER
 
 #colors
@@ -27,11 +27,11 @@ ROOT(){
 #validate using functions
 CHECK(){
     if [ $1 -ne 0 ]
-        then    
-            echo -e "$2 is $R ERROR $N" | tee -a $LOG_FILE
-            exit 1
-        else
-            echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    then    
+        echo -e "$2 is $R ERROR $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 ROOT
@@ -40,11 +40,11 @@ for package in $@
 do 
     dnf list installed $package &>>$LOG_FILE
         if [ $? -ne 0 ]
-            then
-                echo -e "$package is not installed, $R please install $package $N" | tee -a $LOG_FILE
-                dnf install $package -y &>>$LOG_FILE
-                CHECK $? "Installing $package" 
-            else 
-                echo -e "$Y $package is Alraedy instaaled, Please ignore $N" | tee -a $LOG_FILE
+        then
+            echo -e "$package is not installed, $R please install $package $N" | tee -a $LOG_FILE
+            dnf install $package -y &>>$LOG_FILE
+            CHECK $? "Installing $package" 
+        else
+            echo -e "$package is $Y Alraedy instaaled, Please ignore $N" | tee -a $LOG_FILE
         fi
 done
